609bd09d89225e1964ecb3b9606ef2d33650faa0
NikhilAtyali <nikhilatyali90@gmail.com>
2023-06-04 22:42:20 +0530

    Added dynamic wishlist btn

--- src/Pages/ProductList/Component/ProductCard.jsx
+++ src/Pages/ProductList/Component/ProductCard.jsx
@@ -1,6 +1,5 @@
 import StarIcon from "@mui/icons-material/Star";
 import FavoriteIcon from "@mui/icons-material/Favorite";
-
 import "./ProductCard.css";
 import { NavLink } from "react-router-dom";
 import { useContext } from "react";
@@ -9,16 +8,40 @@ import { WishlistContext } from "../../../context/WishListContext";
 function ProductCard({ product }) {
   const { _id, image, productName, rating, price, oldPrice, discount } =
     product;
-  const { addItemToCart } = useContext(CartContext);
-  const { addItemToWishlist } = useContext(WishlistContext);
+  const { cartItems, addItemToCart } = useContext(CartContext);
+  const { wishlistItems, addItemToWishlist, removeFromWishlist } =
+    useContext(WishlistContext);
+
+  const productExistInCart = cartItems.some((item) => item._id === _id);
+  const productExistInWishlist = wishlistItems.some((item) => item._id === _id);
   return (
     <div className="product-card">
-      <button
-        className="add-to-wishlist-btn"
-        onClick={() => addItemToWishlist(product)}
-      >
-        <FavoriteIcon sx={{ color: "grey", "&:hover": { color: "red" } }} />
-      </button>
+      {productExistInWishlist ? (
+        <button
+          onClick={() => addItemToWishlist(product)}
+          className="add-to-wishlist-btn"
+        >
+          <FavoriteIcon
+            sx={{
+              color: "red",
+            }}
+          />
+        </button>
+      ) : (
+        <button
+          onClick={() => removeFromWishlist(_id)}
+          className="add-to-wishlist-btn"
+        >
+          <FavoriteIcon
+            sx={{
+              color: "grey",
+
+              "&:hover": { color: "red" },
+            }}
+          />
+        </button>
+      )}
+
       <NavLink to={`product-details/${_id}`}>
         <img className="product-card__img" src={image} alt="" />
         <div className="heading-rating-container">
@@ -36,14 +59,19 @@ function ProductCard({ product }) {
           <p className="discount">{discount}% OFF</p>
         </div>
       </NavLink>
-      <button
-        className="add-to-cart-btn"
-        onClick={() => addItemToCart(product)}
-      >
-        ADD TO CART
-      </button>
+      {productExistInCart ? (
+        <NavLink to="/cart">
+          <button className="add-to-cart-btn">GO TO CART</button>
+        </NavLink>
+      ) : (
+        <button
+          className="add-to-cart-btn"
+          onClick={() => addItemToCart(product)}
+        >
+          ADD TO CART
+        </button>
+      )}
     </div>
   );
 }
-
 export default ProductCard;
